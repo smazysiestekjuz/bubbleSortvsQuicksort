335c6105def061cc0a3374ef1239f49e793bb591
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-14 12:53:51 +0100

    1. Allocate the array 2. Put random numbers there 3. It will sort by timing

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ int main()
 {
     cout << "Porownanie czasow sortowania v.1" << endl;
 
-    cout <<"Ile liczb w tablicy: ";
+    cout <<"Ile losowych liczb w tablicy: ";
     cin>>ile;
+
+
 
     return 0;
 }
