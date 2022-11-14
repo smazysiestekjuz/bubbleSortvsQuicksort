6b2a465bd5dea1ee0e09a0cc07c5f00872d0783a
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-14 13:09:49 +0100

    Bubble sort algorithm

--- main.cpp
+++ main.cpp
@@ -8,7 +8,19 @@ int ile;
 
 void sortowanie_babelkowe(int *tab, int n)
 {
-
+    for(int i=1; i<n; i++)
+    {
+        for(int j=n-1; j>=1; j--)
+        {
+            if(tab[j]<tab[j-1])
+            {
+                int bufor;
+                bufor=tab[j-1];
+                tab[j-1]=tab[j];
+                tab[j]=bufor;
+            }
+        }
+    }
 }
 
 int main()
