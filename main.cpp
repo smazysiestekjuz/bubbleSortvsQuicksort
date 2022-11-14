d4854d1aa9d7b8196c105f945231da47cb7b0513
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-14 12:51:54 +0100

    Asking user how big the array to be sorted is and storing it in the variable how many

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
+int ile;
+
 int main()
 {
     cout << "Porownanie czasow sortowania v.1" << endl;
+
+    cout <<"Ile liczb w tablicy: ";
+    cin>>ile;
 
     return 0;
 }
